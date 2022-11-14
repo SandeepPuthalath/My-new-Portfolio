985402c45601e291bf30aea674ed812e660accf3
Sandeep <mysisneru@123gmail.com>
2022-11-14 20:31:00 +0530

    portfolio first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// var form = document.getElementById('gform')
+// form.addEventListener('submit',function(event){
+//     event.preventDefault() // prevents form from auto submitting
+
+//     var name = document.getElementById('name').value
+//     console.log(name)
+//     var email = document.getElementById('email').value
+//     console.log(email)
+//     var subject = document.getElementById('subject').value
+//     console.log(subject)
+//     var message = document.getElementById('message').value
+//     console.log(message)
+
+
+// })
